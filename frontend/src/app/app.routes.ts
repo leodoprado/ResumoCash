e1c76c63c34abddeb4fd59ce3978304ee0f3c2d5
Leonardo do Prado <leonardodoprado231@gmail.com>
2026-09-16 21:44:05 -0300

    feat: att

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Landing } from '../app/pages/landing/landing';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Categories } from './pages/categories/categories';
+import { Transactions } from './pages/transactions/transactions';
+import { Profile } from './pages/profile/profile';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Landing,
+    title: 'ResumoCash'
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    title: 'Dashboard | ResumoCash'
+  },
+  {
+    path: 'categories',
+    component: Categories,
+    title: 'Categorias | ResumoCash'
+  },
+  {
+    path: 'transactions',
+    component: Transactions,
+    title: 'Transações | ResumoCash'
+  },
+  {
+    path: 'profile',
+    component: Profile,
+    title: 'Perfil | ResumoCash'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
